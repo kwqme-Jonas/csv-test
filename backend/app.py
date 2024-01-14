21c340f3bca71afa2aea7b5132c5d40d9314537c
Jonas Donkor <donkoronas00@gmail.com>
2024-01-13 18:52:52 -0800

    nade some changes

--- backend/app.py
+++ backend/app.py
@@ -4,17 +4,23 @@ from flask_cors import CORS
 import subprocess
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["http://localhost:3000/submitForm"])
 @app.route('/submitForm', methods=['POST'])
 def submit_form():
     try:
         # Extract data from the form submission
         data = request.json
-        index = data.get('index')
-        addon = data.get('addon')
+        date = data.get('date')
+        contractor = data.get('contractor')
+        welder = data.get('welder')
+        cap_positive = data.get('cap_positive')
+        cap_negative = data.get('cap_negative')
+        district = data.get('district')
+        town = data.get('town')
+        adhesive = data.get('adhesive')
 
         # Execute the Python script
-        result = subprocess.check_output(['python', 'C:\\Users\\User\\Desktop\\test\\excel-extract\\backend', '-index', index, '-addon', addon])
+        result = subprocess.check_output(['python', 'C:\\Users\\User\\Desktop\\test\\excel-extract\\backend', '-date', date, '-contractor', contractor, '-welder', welder, '-cap_positive', cap_positive, '-cap_negative', cap_negative, '-district', district, '-town', town, '-adhesive', adhesive,])
         
         return jsonify({'success': True, 'result': result.decode('utf-8')})
     except Exception as e:

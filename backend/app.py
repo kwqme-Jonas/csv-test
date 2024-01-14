60c75cd3976526e692456ce500003bd1c90fec37
Jonas Donkor <donkoronas00@gmail.com>
2024-01-13 18:10:16 -0800

    updated code base

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 # app.py (Flask server)
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import subprocess
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/submitForm', methods=['POST'])
 def submit_form():
